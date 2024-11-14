bb426cdc153174167c6977e5dfb7a87deade9bbe
Rahul Ramesh Patil <patil.rahu@northeastern.edu>
2024-11-14 00:00:13 -0500

    ASome minor changes in the backend files

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/swiftpayDB'")
+  .connect("mongodb://localhost:27017/swiftpayDB'")
   .then(() => {
     console.log("Connected to MongoDB successfully");
   })
